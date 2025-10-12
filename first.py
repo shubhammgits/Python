ea9d18326c66e87f37c1685329d97af810e271cd
Shubham <shubhamm18.work@gmail.com>
2025-10-12 23:12:34 +0530

    start

--- first.py
+++ first.py
@@ -3,3 +3,11 @@
 """Hello this is
 multiline docstring and not comment!"""
 print("Namaste!")
+
+
+sher = "Cheetah hi kehde"
+
+shubhamKumar = "Camel case"
+ShubhamKumar = "Pascal case"
+shubham_kumar = "Snake case"
+

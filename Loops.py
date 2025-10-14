db8a52d5b5ff84d39500e7066abe2ed104eb45f6
Shubham <shubhamm18.work@gmail.com>
2025-10-14 22:10:55 +0530

    update

---
+++ Loops.py
@@ -0,0 +1,17 @@
+# for i in range(0,21,2):
+#     print(i)
+
+
+
+# for i in range(20):
+#     print(i)
+
+
+
+# for i in range(20, 0, -2):
+#     print(i)
+
+
+
+
+
--- Questions/9.py
+++ Questions/9.py
@@ -3,16 +3,16 @@ temp = int(input("Enter the temprature in degree Celcius: "))
 if(temp<0):
     print("Freezing Cold!")
 
-elif(0<temp and temp<10):
+elif(0<=temp and temp<10):
     print("Very Cold!")
 
-elif(10<temp and temp<20):
+elif(10<=temp and temp<20):
     print("Cold!")
 
-elif(20<temp and temp<30):
+elif(20<=temp and temp<30):
     print("Pleasant!")
 
-elif(30<temp and temp<40):
+elif(30<=temp and temp<=40):
     print("Hot!")
 
 elif(temp>40):

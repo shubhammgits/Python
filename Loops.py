c165621c9b66e5e2b216c105595b2703c17ad80f
Shubham <shubhamm18.work@gmail.com>
2025-10-15 23:56:30 +0530

    update

--- Loops.py
+++ Loops.py
@@ -14,4 +14,25 @@
 
 
 
+# for i in range(-5, -15, -1):
+#     print(i)
+
+
+
+
+
+
+# for i in range(1, 11, 1):
+#     print(f'5 x {i} = ', 5*i)
+
+
+
+# for i in range(5, 50, 5):
+#     print(i)
+
+
+
+n = 89
+for i in range(n, (n*10)+1, n):
+    print(i)
 

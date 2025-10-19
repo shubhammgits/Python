656d094420632f523a8a6b5c9eefe05ee05232ea
Shubham <shubhamm18.work@gmail.com>
2025-10-19 22:05:32 +0530

    update

--- Questions/12.py
+++ Questions/12.py
@@ -1,4 +1,4 @@
 n = int(input("Enter any number :"))
 
-for i in range(1, n+1):
+for i in range(n, 0, -1):
     print(i)
---
+++ Questions/14.py
@@ -0,0 +1,7 @@
+n = int(input("Enter any number :"))
+sum = 0
+
+for i in range(n+1):
+    sum = sum + i
+
+print(f'Sum upto {n} = {sum}')

c69859857d1fa84e7c012be6caaf62d69a614ce6
Shubham <shubhamm18.work@gmail.com>
2025-10-21 15:04:10 +0530

    update

---
+++ Questions/19.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the number :"))
+count = 0
+for i in range(1,n+1):
+    if(n%i == 0):
+        count += 1
+
+if(count > 2):
+    print("Not Prime!")
+else:
+    print("Prime!")

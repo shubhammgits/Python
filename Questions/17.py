3ac39cfc2999d23b114fadfa2a140b340b79f44a
Shubham <shubhamm18.work@gmail.com>
2025-10-21 14:54:33 +0530

    update

--- Questions/17.py
+++ Questions/17.py
@@ -1,6 +1,6 @@
 n = int(input("Enter a number : "))
 
-
-for i in range(n+1):
+print(f"Factors of {n} : ")
+for i in range(1,n+1):
     if(n%i == 0):
-        print(f'Factor of {n} = {i}')
+        print(i)

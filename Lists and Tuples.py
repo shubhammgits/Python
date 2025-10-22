f6c3757ab7d963017a72acb27fee60304246b093
Shubham <shubhamm18.work@gmail.com>
2025-10-22 21:54:59 +0530

    update

--- Lists and Tuples.py
+++ Lists and Tuples.py
@@ -1,7 +1,21 @@
-friends = ["Apple", "Orange", 5, 7.87, False, "Shubham"]
+# friends = ["Apple", "Orange", 5, 7.87, False, "Shubham"]
 
-print(friends[0])
+# print(friends[0])
 
-friends[0] = "Grapes"
+# friends[0] = "Grapes"
 
-print(friends[1:5])
+# print(friends[1:5])
+
+# friends.append("Harry")
+# print(friends)
+
+
+
+nums = [3,6,2,7,8,2]
+nums.sort()
+
+print(nums)
+
+nums.reverse()
+
+print(nums)
